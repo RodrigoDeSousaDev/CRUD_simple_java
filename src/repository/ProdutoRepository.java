280a244bae585bc042334c49fa0038f505be998f
RodrigoDeSousaDev <rodrigoprofissional912@hotmail.com>
2025-11-24 19:00:22 -0300

    setting methods CRUD

--- src/repository/ProdutoRepository.java
+++ src/repository/ProdutoRepository.java
@@ -1,4 +1,30 @@
 package repository;
 
+import model.Produto;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProdutoRepository {
+    private List<Produto> produtos = new ArrayList<>();
+
+    public void salvar(Produto produto){
+        produtos.add(produto);
+    }
+
+    public List<Produto> Listar(){
+        return produtos;
+    }
+
+    public Produto buscarPorId(int id){
+        for (Produto p : produtos){
+            if (p.getId()==id){
+                return p;
+            }
+        }
+        return null;
+    }
+
+    public boolean remover(int id){
+        return produtos.removeIf(p -> p.getId() == id);
+    }
 }
