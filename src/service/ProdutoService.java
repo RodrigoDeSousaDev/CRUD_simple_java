13fae92cf8b479b136f52d01d3ba77ec049c5bbe
RodrigoDeSousaDev <rodrigoprofissional912@hotmail.com>
2025-11-24 19:07:41 -0300

    method for registering created

--- src/service/ProdutoService.java
+++ src/service/ProdutoService.java
@@ -1,4 +1,22 @@
 package service;
 
+import model.Produto;
+import repository.ProdutoRepository;
+
+import java.util.Random;
+
+
 public class ProdutoService {
+    private ProdutoRepository repository = new ProdutoRepository();
+
+    public void cadastrar(String nome, double preco){
+        Random random = new Random();
+        int id = random.nextInt(9000)+ 1000;
+
+        Produto p = new Produto(id, nome, preco);
+        repository.salvar(p);
+
+        System.out.println("Produto cadastrado com sucesso");
+    }
+
 }
