8738b25b70eefb71fa13973ae180ba75b923eb98
RodrigoDeSousaDev <rodrigoprofissional912@hotmail.com>
2025-11-24 19:31:09 -0300

    Finish simple CRUD

--- src/App.java
+++ src/App.java
@@ -16,8 +16,45 @@ public class App {
             System.out.println("5 - Sair");
             System.out.println("--------------------");
 
-            int opcoes = scanner.nextInt();
+            int opcao = scanner.nextInt();
             scanner.nextLine();
+
+            if (opcao == 1){
+                System.out.println("Nome: ");
+                String nome = scanner.nextLine();
+
+                System.out.println("Preço: ");
+                double preco = scanner.nextDouble();
+
+                service.cadastrar(nome, preco);
+
+            }else if(opcao == 2){
+                service.listar();
+
+            }else if (opcao == 3){
+                System.out.println("ID do produto: ");
+                int id = scanner.nextInt();
+                scanner.nextLine();
+
+                System.out.println("Novo nome: ");
+                String novoNome = scanner.nextLine();
+
+                System.out.println("Novo preco: ");
+                double novoPreco = scanner.nextDouble();
+
+                service.atualizar(id, novoNome, novoPreco);
+            }else if(opcao == 4){
+                System.out.println("Id do produto: ");
+                int id = scanner.nextInt();
+
+                service.remover(id);
+            }else if(opcao == 5){
+                System.out.println("FIM DO MEU CRUD");
+                break;
+            }else{
+                System.out.println("Opção invalida");
+            }
         }
+        scanner.close();
     }
 }
